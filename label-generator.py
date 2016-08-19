1d24a606644c214125c09c32ee3ed6ba28d379e4
Talon Gonyeau <gonyeau@gravitec.local>
2016-08-19 16:02:13 -0700

    Added Textbox - 60%

--- label-generator.py
+++ label-generator.py
@@ -1,4 +1,5 @@
 from PIL import Image, ImageDraw, ImageFont
+import textwrap
 
 ''' TODO:
 Add text-wrapping
@@ -11,9 +12,13 @@ PIXELS_PER_INCH = 300
 PAPER_SIZE = (8.5, 11)
 ROWS = 5
 COLUMNS = 2
+
 BORDER_WIDTH = 40
 LOGO_MARGIN = 40
-TEXT_MARGIN = 25
+TOP_MARGIN = .5
+SIDE_MARGIN = .75
+TEXT_MARGIN = 30
+
 IMAGE_BACKGROUND = "white"
 logo = Image.open("fair_logo.jpg")
 logo = logo.resize((369, 273), Image.LANCZOS) #328, 243
@@ -23,23 +28,37 @@ FONT_PATH = "times.ttf"
 IMAGE_SIZE = (int(PAPER_SIZE[0] * PIXELS_PER_INCH),
               int(PAPER_SIZE[1] * PIXELS_PER_INCH))
 MARGIN_SIZE = 1 * PIXELS_PER_INCH
-TOP_MARGIN = int(.5 * PIXELS_PER_INCH)
-SIDE_MARGIN = int(.75 * PIXELS_PER_INCH)
+TOP_MARGIN = int(TOP_MARGIN * PIXELS_PER_INCH)
+SIDE_MARGIN = int(SIDE_MARGIN * PIXELS_PER_INCH)
+
 X_MIN = SIDE_MARGIN
 X_MAX = IMAGE_SIZE[0] - SIDE_MARGIN
 Y_MIN = TOP_MARGIN
 Y_MAX = IMAGE_SIZE[1] - TOP_MARGIN
+
 PIXELS_IN_ROW = ((Y_MAX - Y_MIN) // ROWS)
 CARD_SIZE = (((X_MAX - X_MIN) // 2), PIXELS_IN_ROW)
-LOGO_TRANSLATION = (LOGO_MARGIN, CARD_SIZE[1] - logo.size[1] - LOGO_MARGIN)
-TITLE_TRANSLATION = (TEXT_MARGIN, TEXT_MARGIN)
-NAME_TRANSLATION = (logo.size[0] + LOGO_MARGIN * 2, CARD_SIZE[1] - TEXT_MARGIN * 2 - logo.size[1] )
+
 font10 = ImageFont.truetype(FONT_PATH, 100)
 font8 = ImageFont.truetype(FONT_PATH, 100)
 
-
 image = Image.new("RGB", IMAGE_SIZE, IMAGE_BACKGROUND)
 draw = ImageDraw.Draw(image, "RGB")
+
+LOGO_TRANSLATION = (LOGO_MARGIN, CARD_SIZE[1] - logo.size[1] - LOGO_MARGIN)
+# Format        [(UPPER, LEFT), (LOWER, RIGHT), (SIZE)]
+title_textbox = [(TEXT_MARGIN + X_MIN, TEXT_MARGIN + Y_MIN),
+                 (CARD_SIZE[0] - TEXT_MARGIN + X_MIN, CARD_SIZE[1] - logo.size[1] - LOGO_MARGIN * 2 - TEXT_MARGIN + Y_MIN),
+                 ()]
+title_textbox[2] = (title_textbox[1][0] - title_textbox[0][0], title_textbox[1][1] - title_textbox[0][1])
+name_textbox = [(logo.size[0] + LOGO_MARGIN * 2 + X_MIN, CARD_SIZE[1] - logo.size[1] - LOGO_MARGIN * 2 + TEXT_MARGIN + Y_MIN),
+                (CARD_SIZE[0] - TEXT_MARGIN * 2 + X_MIN, CARD_SIZE[1] - TEXT_MARGIN * 2 + Y_MIN),
+                ()]
+name_textbox[2] = (name_textbox[1][0] - name_textbox[0][0], name_textbox[1][1] - name_textbox[0][1])
+    
+#TO REMOVE - DEPRECIATED
+TITLE_TRANSLATION = (TEXT_MARGIN, TEXT_MARGIN)
+NAME_TRANSLATION = (logo.size[0] + LOGO_MARGIN * 2, CARD_SIZE[1] - TEXT_MARGIN * 2 - logo.size[1] )
 
 #Generates a list containing the upper left coordinate of each card.
 cards_coordinates = []
@@ -90,15 +109,39 @@ current_data = [ #[Name, Title, Name_Modifier, Title Modifier]
     ["George Martin", "Painting of a Cat"], 
     ["George Martin", "Painting of a Dog"],
     ["George Martin", "Painting of a Train"],
-    ["Perry the Platypus", "Bach is Nude"],
-    ["Perry the Platypus", "Nudity"],
+    ["Perry the Platypus", "Bach is Nice"],
+    ["Perry the Platypus", "Nicity"],
     ["Perry the Platypus", "The French Explorer,\n Bachurat the Brave"],
     ["San Jean Von Eisenvon", "123"],
     ["San Jean Von Eisenvon", "456"],
     ["San Jean Von Eisenvon", "789"],
-    ["San Jean Von Eisenvon", "0"]]
+    ["San Jean Von Eisenvon", "022"]]
 
-#draw.multiline_textsize("Perry the Platypus",font=font10, spacing=0)
+def text_format(text, text_box_size, font):
+    text_size = draw.multiline_textsize(text, font=font, spacing=6)
+    
+    if text_size >= text_box_size:
+        number_of_lines = 2
+    else:
+        number_of_lines = 1
+    
+    
+    text_center(text, text_box_size, font, number_of_lines)
+
+def text_center(text, text_box_size, font, number_of_lines):
+    text_size = draw.multiline_textsize(text, font=font, spacing=6)
+    if text_size >= text_box_size:
+        pass
+    else:
+        pass
+
+    formatted_text = text
+    
+    xy_adjustment = (0,0)
+
+    return formatted_text, xy_adjustment
+
+
 def write_text(data_list):
     image = blank_image
     for i, coord in enumerate(cards_coordinates):
@@ -109,7 +152,13 @@ def write_text(data_list):
         current_coord = (coord[0] + NAME_TRANSLATION[0], coord[1] + NAME_TRANSLATION[1])
         draw.multiline_text((current_coord), data_list[i][0], fill="black",
                             font=font8, spacing=6, align="center" )
-        
+
+
+#TO REMOVE, Locates name and title textboxes
+draw.line(title_textbox[0:2],
+            fill="black", width=5)
+draw.line(name_textbox[0:2],
+            fill="black", width=5)
 
 write_text(current_data)
 image.save("Test.png")
